2a8567d9d398ac288f0f3cfd40cc71930b561e75
adabbott <adabbott@uga.edu>
2018-02-08 16:07:43 -0500

    generalized replacement of cartesian coordinates now appears to be working

--- regex_tests/regex.py
+++ regex_tests/regex.py
@@ -5,7 +5,6 @@ import re
 
 # save some common regex features as human readable variables
 letter           = r'[a-zA-Z]' 
-unsigned_integer = r'\d+'      
 double           = r'-?\d+\.\d+'
 whitespace       = r'\s'       
 endline          = r'\n'       
@@ -19,23 +18,24 @@ def maybe(string):
     return r'(?:{:s})?'.format(string)
 
 def two_or_more(string):                 
-  return r'(?:{:s}){{2,}}'.format(string)
+    """
+    A regex wrapper for an arbitrary string.
+    Allows multiple instances of the string to be matched.
+    """
+    return r'(?:{:s}){{2,}}'.format(string)
 
-print(maybe(letter))
+# zero or more whitespace (ws) followed by the regex feature 
+ws_double  = r"\s*" + double    # for floats
+ws_endline = r"\s*" + endline   # for newlines 
 
-
-
-# zero or more whitespace followed by x 
-ws_double  = r"\s*" + double 
-ws_endline = r"\s*" + endline 
-
-# an xyz style geometry line, atom  x   y  z
-geom_line_regex = r"\s*" + letter + maybe(letter) + 3 * ws_double + ws_endline
+# a regex identifier for an xyz style geometry line, atom_label  x_coord   y_coord  z_coord
+geom_line_regex = r"[ \t]*" + letter + maybe(letter) + 3 * ws_double + ws_endline
 # an xyz style geometry block
 geom_block_regex = two_or_more(geom_line_regex)
+print(geom_block_regex)
 
-# test regex identifier for robustness
-xyz1 = " C  0.0000 2.000 1.000  \n Cl  2.0000 1.000 0.000 \n BR  1.0000 0.000 2.000"
+# test regex identifier with xyz string
+xyz1 = " C  0.0000 2.000 1.000  \n Cl  2.0000 1.000 0.000 \n Br  1.0000 0.000 2.000 \n"
 
 if re.match(geom_block_regex, xyz1):
     print('Yes')
@@ -43,3 +43,11 @@ else:
     print('No')
 
 
+# test pulling from a sample input file and writing new input file
+with open('input.dat', 'r') as f:
+    data = f.read()
+
+new = re.sub(geom_block_regex, xyz1, data)
+
+with open('input2.dat', 'w+') as f:
+    f.write(new)
