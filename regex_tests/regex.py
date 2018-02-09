56acee268fdb89c7dd96cb7e2d7184be45f546f3
adabbott <adabbott@uga.edu>
2018-02-09 18:15:41 -0500

    played around with more regex

--- regex_tests/regex.py
+++ regex_tests/regex.py
@@ -6,14 +6,14 @@ import re
 # save some common regex features as human readable variables
 letter           = r'[a-zA-Z]' 
 double           = r'-?\d+\.\d+'
+integer          = r'\d+'
 whitespace       = r'\s'       
 endline          = r'\n'       
                                
 
 def maybe(string):
     """
-    A regex wrapper for an arbitrary string.
-    Allows a string to be present, but still matches if it is not present.
+    A regex wrapper for an arbitrary string.  Allows a string to be present, but still matches if it is not present.
     """
     return r'(?:{:s})?'.format(string)
 
@@ -32,22 +32,64 @@ ws_endline = r"\s*" + endline   # for newlines
 geom_line_regex = r"[ \t]*" + letter + maybe(letter) + 3 * ws_double + ws_endline
 # an xyz style geometry block
 geom_block_regex = two_or_more(geom_line_regex)
-print(geom_block_regex)
 
 # test regex identifier with xyz string
 xyz1 = " C  0.0000 2.000 1.000  \n Cl  2.0000 1.000 0.000 \n Br  1.0000 0.000 2.000 \n"
-
-if re.match(geom_block_regex, xyz1):
-    print('Yes')
-else:
-    print('No')
 
 
 # test pulling from a sample input file and writing new input file
 with open('input.dat', 'r') as f:
     data = f.read()
 
-new = re.sub(geom_block_regex, xyz1, data)
+print(data)
 
-with open('input2.dat', 'w+') as f:
-    f.write(new)
+#print(re.match(geom_block_regex, data))
+#new = re.sub(geom_block_regex, xyz1, data)
+#
+#with open('input2.dat', 'w+') as f:
+#    f.write(new)
+
+#xyz_block_regex = geom_block_regex
+#
+## if there is more than one xyz, take the last one
+#iter_matches = re.finditer(xyz_block_regex, data, re.MULTILINE)
+#matches = [match for match in iter_matches]
+#if matches is None:
+#    raise Exception("No XYZ geometry found in template input file")
+#
+#start = matches[-1].start()
+#end   = matches[-1].end()
+#
+#xyz = data[start:end]
+
+
+#########################
+# begin attempt to parse compact internal coordinates
+#########################
+"""
+O
+H 1 1.0
+H 1 1.0 2 104.5
+H 1 1.0 2 100.00 3 180.0
+"""
+
+integer    = r'\d+'
+ws_int     = r"\s*" + integer  # for integers
+
+int_line1_regex = letter + maybe(letter) + ws_endline
+int_line2_regex = letter + maybe(letter) + ws_int + ws_double + ws_endline
+int_line3_regex = letter + maybe(letter) + ws_int + ws_double + ws_int + ws_double + ws_endline
+int_line4_regex = letter + maybe(letter) + ws_int + ws_double + ws_int + ws_double + ws_int + ws_double + ws_endline
+
+def one_or_more(string):                 
+    """
+    A regex wrapper for an arbitrary string.
+    Allows multiple instances of the string to be matched.
+    """
+    return r'(?:{:s}){{1,}}'.format(string)
+
+test_regex = int_line1_regex + int_line2_regex + maybe(int_line3_regex) + maybe(one_or_more(int_line4_regex))
+
+print(re.search(test_regex, data))
+
+
