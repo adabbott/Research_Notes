82ede11259b0d83967e0b0db36636348b1497330
adabbott <adabbott@uga.edu>
2018-03-13 13:29:48 -0400

    added cclib import test

---
+++ cclib/import_test.py
@@ -0,0 +1,9 @@
+# cclib cloned from Github, installed with:
+# python setup.py build
+# python setup.py install 
+
+# attempt to parse a psi4 output file with cclib
+
+# ccio class contains ccwrite, ccread, ccopen, etc, which all claim to automatically determine output file type
+import cclib.io.ccio as ccio 
+
