8c8247c9caf6f58b6bec6da03185d28a43d71f7e
adabbott <adabbott@uga.edu>
2019-01-26 16:42:58 -0500

    finished xyz parser

--- new_coordinate_reader/read.py
+++ new_coordinate_reader/read.py
@@ -2,13 +2,14 @@ import peslearn
 import re
 import collections
 import numpy as np
+import pandas as pd
 
 xyz_re = peslearn.regex.xyz_block_regex 
 atom_label = peslearn.regex.xyz_block_regex 
 
 with open('OCHCO_7800.xyz') as f:
-    # read it
     data = f.read()
+
 # extract energies
 energies = re.findall("\n\s*(-?\d+\.\d+)\s*\n", data)
 # extract geometries and clean up format
@@ -19,7 +20,7 @@ for i in range(len(geoms)):
 sample = geoms[0]
 atom_labels = [re.findall('\w+', s)[0] for s in sample]
 natoms = len(atom_labels)
-# convert atom labels to standard order
+# convert atom labels to standard order (most common element first, alphabetical tiebreaker)
 sorted_atom_counts = collections.Counter(atom_labels).most_common() 
 sorted_atom_counts = sorted(sorted_atom_counts, key = lambda x: (-x[1], x[0]))
 sorted_atom_labels = []
@@ -34,26 +35,45 @@ for i,j in enumerate(atom_labels):
             p.append(k)
             sorted_atom_labels[k] = 'done'
             continue
-
 # permute all xyz geometries to standard order 
 for g in range(len(geoms)):
     geoms[g] = [geoms[g][i] for i in p]
 
-# write new xyz with standard order
-with open('new_' + 'OCHCO_7800.xyz', 'w+') as f:
+# write new xyz file with standard order
+with open('std_' + 'OCHCO_7800.xyz', 'w+') as f:
     for i in range(len(energies)):
         f.write(energies[i] +'\n')  
         for j in range(natoms):
             f.write(geoms[i][j] +'\n') 
 
-# remove everything from XYZs except floats                 
-#for i,g in enumerate(geoms):
-#    for j,s in enumerate(g):
-#        s = s.split()
-#        del s[0]
-#        g[j] = s
-#        print(s)#g = [a.split() for a in g]
-#print(np.asarray(geoms[0], dtype=np.float32))
+# remove everything from XYZs except floats and convert to numpy arrays
+for i,geom in enumerate(geoms):
+    for j,string in enumerate(geom):
+        string = string.split()
+        del string[0] # remove atom label
+        geom[j] = np.asarray(string, dtype=np.float32)
+
+# convert to interatomic distances
+final_geoms = []
+for i in geoms:
+    idm = peslearn.geometry_transform_helper.get_interatom_distances(i)                        
+    idm = idm[np.tril_indices(idm.shape[0],-1)]                 
+    final_geoms.append(idm)
+
+final_geoms = np.asarray(final_geoms) 
+energies = np.asarray(energies, dtype=np.float32)
+
+n_interatomics =  int(0.5 * (natoms * natoms - natoms))
+bond_columns = []
+for i in range(n_interatomics):
+    bond_columns.append("r%d" % (i))
+DF = pd.DataFrame(data=final_geoms, columns=bond_columns)    
+DF['E'] = energies                                               
+DF.to_csv('PES.dat',index=False,  float_format='%12.10f')
+ 
+ 
+                                          
+
     
     
 
