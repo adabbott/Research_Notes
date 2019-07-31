a8e047a3debeab446a5090e66fa0b8f3a4208e23
adabbott <adabbott@uga.edu>
2019-07-31 14:08:57 -0400

    cleaned up tests and autodiff file

---
+++ autodiffBtensor/ad_btensor.py
@@ -0,0 +1,32 @@
+import psi4
+import torch
+import ad_intcos
+import ad_v3d
+import numpy as np
+torch.set_printoptions(threshold=5000, linewidth=200, precision=10)
+np.set_printoptions(threshold=5000, linewidth=200, precision=10)
+bohr2ang = 0.529177249
+
+def autodiff_Btensor(intcos, geom, order=1):
+    """
+    Given internal coordinate definitions and Cartesian geometry, compute the order'th B Tensor
+    with PyTorch automatic differentiation. Beyond third order gets really expensive. 
+    """
+    B = ad_intcos.qValues(intcos, geom)   # Generate internal coordinates from cartesians
+    nint = B.shape[0]
+    ncart = 3 * geom.shape[0]
+    count = 0
+    shape = [nint, ncart]
+    while count < order:
+        gradients = []
+        for val in B.flatten():
+            if count + 1 != order: 
+                g = torch.autograd.grad(val, geom, create_graph=True)[0].reshape(ncart)
+            else: # Save time: for last B tensor, don't create derivative graph.
+                g = torch.autograd.grad(val, geom, retain_graph=True)[0].reshape(ncart)
+            gradients.append(g)
+        B = torch.stack(gradients).reshape(tuple(shape)) # shape into final B tensor
+        shape.append(ncart)                              # update shape for next B tensor
+        count += 1                              
+    return B
+
