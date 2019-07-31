4da66f5bf6392df5625b049d16506dc5d97d7a0b
adabbott <adabbott@uga.edu>
2019-07-31 16:06:48 -0400

    added experimental efficient B tensor computation. New functions for computing q in each itnernal coordinate class have been added to facilitate this

--- autodiffBtensor/ad_intcos.py
+++ autodiffBtensor/ad_intcos.py
@@ -35,6 +35,10 @@ class STRE(object):
     def q(self, geom):
         return ad_v3d.dist(geom[self.A], geom[self.B])
 
+    def new_q(self, coord1, coord2):
+        return ad_v3d.dist(coord1, coord2)
+        
+
 class BEND(object):
     def __init__(self, a, b, c, bendType="REGULAR"):
         if a < c:
@@ -44,20 +48,25 @@ class BEND(object):
         self.A = atoms[0]
         self.B = atoms[1]
         self.C = atoms[2]
-        self._bendType = bendType # just "REGULAR" bends for now, no linear or complimentary
+        self._bendType = bendType 
         self._axes_fixed = False
 
     def compute_axes(self, geom):
         u = ad_v3d.eAB(geom[self.B], geom[self.A])  # B->A
         v = ad_v3d.eAB(geom[self.B], geom[self.C])  # B->C
 
+        if ad_v3d.are_parallel_or_antiparallel(u,v):
+            self._bendType = "LINEAR"
+
         if self._bendType == "REGULAR":                   # not a linear-bend type
             self._w = ad_v3d.normalize(ad_v3d.cross(u,v)) # cross product and normalize
             self._x = ad_v3d.normalize(u + v)             # angle bisector
             return
 
-        tv1 = torch.tensor([1,0,0], dtype=torch.float64, requires_grad=True) 
-        tmp_tv2 = torch.tensor([0,1,1], dtype=torch.float64, requires_grad=True)
+        #tv1 = torch.tensor([1,0,0], dtype=torch.float64, requires_grad=True) 
+        tv1 = torch.tensor([1,0,0], dtype=torch.float64) 
+        #tmp_tv2 = torch.tensor([0,1,1], dtype=torch.float64, requires_grad=True)
+        tmp_tv2 = torch.tensor([0,1,1], dtype=torch.float64)
         tv2 = ad_v3d.normalize(tmp_tv2)
 
         u_tv1 = ad_v3d.are_parallel_or_antiparallel(u, tv1)
@@ -89,12 +98,66 @@ class BEND(object):
             self.compute_axes(geom)
         u = ad_v3d.eAB(geom[self.B], geom[self.A])  # B->A
         v = ad_v3d.eAB(geom[self.B], geom[self.C])  # B->C
-        origin = torch.zeros(3, dtype=torch.float64, requires_grad=True)
+        #origin = torch.zeros(3, dtype=torch.float64, requires_grad=True)
+        origin = torch.zeros(3, dtype=torch.float64)
         phi1 = ad_v3d.angle(u, origin, self._x) 
         phi2 = ad_v3d.angle(self._x, origin, v)
         phi = phi1 + phi2
         return phi
 
+    def new_q(self, coord1, coord2, coord3):
+        if not self._axes_fixed:
+            self.new_compute_axes(coord1,coord2,coord3)
+        u = ad_v3d.eAB(coord2, coord1)  # B->A
+        v = ad_v3d.eAB(coord2, coord3)  # B->C
+        origin = torch.zeros(3, dtype=torch.float64)
+        phi1 = ad_v3d.angle(u, origin, self._x) 
+        phi2 = ad_v3d.angle(self._x, origin, v)
+        phi = phi1 + phi2
+        return phi
+
+    def new_compute_axes(self, coord1,coord2,coord3):
+        u = ad_v3d.eAB(coord2, coord1)  # B->A
+        v = ad_v3d.eAB(coord2, coord3)  # B->C
+
+        if ad_v3d.are_parallel_or_antiparallel(u,v):
+            self._bendType = "LINEAR"
+
+        if self._bendType == "REGULAR":                   # not a linear-bend type
+            self._w = ad_v3d.normalize(ad_v3d.cross(u,v)) # cross product and normalize
+            self._x = ad_v3d.normalize(u + v)             # angle bisector
+            return
+
+        #tv1 = torch.tensor([1,0,0], dtype=torch.float64, requires_grad=True) 
+        tv1 = torch.tensor([1,0,0], dtype=torch.float64) 
+        #tmp_tv2 = torch.tensor([0,1,1], dtype=torch.float64, requires_grad=True)
+        tmp_tv2 = torch.tensor([0,1,1], dtype=torch.float64)
+        tv2 = ad_v3d.normalize(tmp_tv2)
+
+        u_tv1 = ad_v3d.are_parallel_or_antiparallel(u, tv1)
+        v_tv1 = ad_v3d.are_parallel_or_antiparallel(v, tv1)
+        u_tv2 = ad_v3d.are_parallel_or_antiparallel(u, tv2)
+        v_tv2 = ad_v3d.are_parallel_or_antiparallel(v, tv2)
+
+        # handle both types of linear bends
+        if not ad_v3d.are_parallel_or_antiparallel(u, v):
+            self._w = ad_v3d.normalize(ad_v3d.cross(u, v))  # orthogonal vector
+            self._x = ad_v3d.normalize(u + v)
+        # u || v but not || to tv1.
+        elif not u_tv1 and not v_tv1:
+            self._w = ad_v3d.normalize(ad_v3d.cross(u, tv1))
+            self._x = ad_v3d.normalize(ad_v3d.cross(self._w, u))
+        # u || v but not || to tv2.
+        elif not u_tv2 and not v_tv2:
+            self._w = ad_v3d.normalize(ad_v3d.cross(u,tv2))
+            self._x = ad_v3d.normalize(ad_v3d.cross(self._w, u))
+
+        if self._bendType == "COMPLEMENT":
+            w2 = torch.copy(self._w)
+            self._w = -1.0 * self._x  # -w_normal -> x_complement
+            self._x = w2
+        return
+          
 
 class TORS(object):
     def __init__(self, a, b, c, d):
@@ -107,43 +170,18 @@ class TORS(object):
         self.D = self.atoms[3]
         self._near180 = 0
 
-    def near180(self):
-        return self._near180
-
-    def zeta(a, m, n):
-        if a == m: 
-            return 1
-        elif a == n: 
-            return -1
-        else: 
-            return 0
-
-    def updateOrientation(self, geom):
-        tval = self.q(geom)
-        if tval > op.Params.fix_val_near_pi:
-            self._near180 = +1
-        elif tval < -1 * op.Params.fix_val_near_pi:
-            self._near180 = -1
-        else:
-            self._near180 = 0
-        return
-
-
     def q(self, geom):
         try:
-            #print("TORSION ORDER", self.A, self.B, self.C, self.D)
             tau = ad_v3d.tors(geom[self.A], geom[self.B], geom[self.C], geom[self.D])
         except: 
             raise Exception("Tors.q: unable to compute torsion value")
+        return tau
 
-        # Extend values domain of torsion angles beyond pi or -pi, so that
-        # delta(values) can be calculated
-        if self._near180 == -1 and tau > fix_val_near_pi:
-            return tau - 2.0 * math.pi
-        elif self._near180 == +1 and tau < -1 * fix_val_near_pi:
-            return tau + 2.0 * math.pi
-        else:
-            return tau
-
+    def new_q(self, coord1, coord2, coord3, coord4):
+        try:
+            tau = ad_v3d.tors(coord1, coord2, coord3, coord4)
+        except: 
+            raise Exception("Tors.q: unable to compute torsion value")
+        return tau
 
 
