37d09688fb2da9f16dde211c6cd92ea5f98410a3
adabbott <adabbott@uga.edu>
2019-08-02 13:10:22 -0400

    psi4 and NN hessians match

--- ML_forceconstants/water_1/derivatives.py
+++ ML_forceconstants/water_1/derivatives.py
@@ -1,10 +1,14 @@
+import psi4
 from peslearn.ml import NeuralNetwork
 from peslearn import InputProcessor
 import numpy as np
 import torch
 from compute_energy import pes
 import Btensors
+np.set_printoptions(threshold=5000, linewidth=200, precision=5)
+torch.set_printoptions(threshold=5000, linewidth=200, precision=5)
 
+# Load NN model
 nn = NeuralNetwork('model_data/PES.dat', InputProcessor(''), molecule_type='A2B')
 params = {'layers': (64, 64), 'morse_transform': {'morse': True, 'morse_alpha': 1.2000000000000002}, 'pip': {'degree_reduction': False, 'pip': True}, 'scale_X': {'activation': 'tanh', 'scale_X': 'mm11'}, 'scale_y': 'std', 'lr': 0.8}
 X, y, Xscaler, yscaler =  nn.preprocess(params, nn.raw_X, nn.raw_y)
@@ -12,17 +16,17 @@ model = torch.load('model_data/model.pt')
 
 def transform(interatomics):
     """ Takes Torch Tensor (requires_grad=True) of interatomic distances, manually transforms geometry to track gradients, computes energy
-        Hard-coded based on hyperparameters """
-    #inp1 = torch.tensor(interatomics, dtype=torch.float64, requires_grad=True)
+        Hard-coded based on hyperparameters above. Returns: energy in units the NN model was trained on"""
     inp2 = -interatomics / 1.2
     inp3 = torch.exp(inp2)
     inp4 = torch.stack((inp3[0], inp3[1] + inp3[2], torch.sum(torch.pow(inp3[1:],2))), dim=0) # Careful! Degree reduce?
     inp5 = (inp4 * torch.tensor(Xscaler.scale_, dtype=torch.float64)) + torch.tensor(Xscaler.min_, dtype=torch.float64)
     out1 = model(inp5)
-    out2 = (out1 * torch.tensor(yscaler.scale_, dtype=torch.float64)) + torch.tensor(yscaler.mean_, dtype=torch.float64)
-    return out2
+    energy = (out1 * torch.tensor(yscaler.scale_, dtype=torch.float64)) + torch.tensor(yscaler.mean_, dtype=torch.float64)
+    return energy
 
 def cart2distances(cart):
+    """Transforms cartesian coordinate torch Tensor (requires_grad=True) into interatomic distances"""
     natom = cart.size()[0]
     ndistances = int((natom**2 - natom) / 2)
     distances = torch.zeros((ndistances), requires_grad=True, dtype=torch.float64)
@@ -33,8 +37,26 @@ def cart2distances(cart):
                 distances[count] = torch.norm(cart[i]- cart[j])
                 count += 1
     return distances
-    
 
+def differentiate_nn(energy, geometry, order=1):
+    # The grad_tensor starts of as a single element, the energy. Then it becomes the gradient, hessian, cubic ... 
+    # depeneding on value of 'order'
+    grad_tensor = energy
+    # The number of geometry parameters. Returned gradient tensor will have this size in all dimensions
+    nparams = torch.numel(geometry)
+    # The shape of first derivative. Will be adjusted at higher order
+    shape = [nparams]
+    count = 0
+    while count < order:
+        gradients = []
+        for value in grad_tensor.flatten():
+            g = torch.autograd.grad(value, geometry, create_graph=True)[0].reshape(nparams)
+            gradients.append(g)
+        grad_tensor = torch.stack(gradients).reshape(tuple(shape))
+        shape.append(nparams)
+        count += 1
+    return grad_tensor
+    
 # this is optimized tightly, but apparently model was not trained on true equilibrium, just approximate. Next geom is true geom the model was trained on
 #cartesians = torch.tensor([[0.0000000000,  1.0144292965, -0.0959637982],
 #                           [0.0000000000, -0.0959637982,  1.0144292965],
@@ -50,73 +72,37 @@ print("Equilibrium geometry and energy: ", eq_geom, pes(eq_geom,cartesian=False)
 
 # Compute internal coordinate Hessian wrt starting from both interatomic distances and cartesian coordinates, this works
 E = transform(distances)
-g = torch.autograd.grad(E, distances, create_graph=True)[0]
-h1 = torch.autograd.grad(g[0], distances, create_graph=True)[0]
-h2 = torch.autograd.grad(g[1], distances, create_graph=True)[0]
-h3 = torch.autograd.grad(g[2], distances, create_graph=True)[0]
-F = torch.stack([h1,h2,h3])
-print("Pytorch internal coordinate Hessian:\n", F)
+g =  differentiate_nn(E, distances, order=1)
+h =  differentiate_nn(E, distances, order=2)
+print("Pytorch internal coordinate Hessian:\n", h)
 
 computed_distances = cart2distances(cartesians)
-print(computed_distances)
 E = transform(computed_distances)
-g = torch.autograd.grad(E, computed_distances, create_graph=True)[0]
-h1 = torch.autograd.grad(g[0], computed_distances, create_graph=True)[0]
-h2 = torch.autograd.grad(g[1], computed_distances, create_graph=True)[0]
-h3 = torch.autograd.grad(g[2], computed_distances, create_graph=True)[0]
-F = torch.stack([h1,h2,h3])
-print("Pytorch internal coordinate Hessian:\n", F)
+g =  differentiate_nn(E, computed_distances, order=1)
+h2 =  differentiate_nn(E, computed_distances, order=2)
+print("Pytorch internal coordinate Hessian:\n", h2)
 
-# Compute Cartesian hessian
-computed_distances = cart2distances(cartesians)
-E = transform(computed_distances)
-g = torch.autograd.grad(E, cartesians, create_graph=True)[0]
-print('Cartesian gradient', g)
+hcart =  differentiate_nn(E, cartesians, order=2)
+print("Pytorch Cartesian Hessian:\n", hcart)
 
-#test = torch.autograd.grad(tmp_distances[0], cartesians, create_graph=True)[0]
-#print(test)
+psi4.core.be_quiet()
+h2o = psi4.geometry(
+'''
+H 0.0000000000 0.0000000000 0.9496765298
+H 0.0000000000 0.8834024755 -0.3485478124
+O 0.0000000000 0.0000000000 0.0000000000
+no_com
+no_reorient
+''')
 
-#g = torch.autograd.grad(out2, inp1, create_graph=True)[0]
-#print("Pytorch Gradient:\n", g)
-##
-## The second derivatives of the derivative of the first input.
-#h1 = torch.autograd.grad(g[0], inp1, create_graph=True)[0]
-## The second derivatives of the derivative of the second input
-#h2 = torch.autograd.grad(g[1], inp1, create_graph=True)[0]
-#h3 = torch.autograd.grad(g[2], inp1, create_graph=True)[0]
-### The Hessian, [d^2y/dx1^2, d^2y/dx1dx2]
-###              [d^2y/dx2dx1, d^2y/dx2^2]
-#F = torch.stack([h1,h2,h3])
-#print("Pytorch internal coordinate Hessian:\n", F)
-
-# H H O
-# Transform cartesians into interatomic distances
-
-#G = torch.tensor([[ 1.05475559, 0.05223459, 0.82900085],
-#                  [ 0.05223459, 1.05475559,-0.39300449],
-#                  [ 0.82900085,-0.39300449, 1.98447145]], dtype=torch.float64)
-#
-#GF = torch.matmul(G, F * 4.35974)
-#
-#lamda = torch.symeig(GF)[0]
-#lamda = lamda  * 1e-18 * (1 / 1.66054e-27) * (1e10)**2
-#hertz = torch.sqrt(lamda) / (2* np.pi)
-#hz2cm = 3.33565e-11
-#frequencies = hertz*hz2cm
-#print(frequencies)
+psi4.set_options({'scf_type': 'pk'})
+h, wfn = psi4.hessian('scf/6-31g', return_wfn = True)
+hess = np.array(h)
+# convert hessian to angstrom
+hess /= 0.529177249**2
+print(hess)
 
 
-## Manually tranform to track gradients: 1. Initial geometry 2. Morse transform 3. PIP transform 4. Scale transform
-## Compute energy, and reverse scaling 
-#inp1 = torch.tensor(eq_geom, dtype=torch.float64, requires_grad=True)
-#inp2 = -inp1 / 1.2
-#inp3 = torch.exp(inp2)
-## Careful! Degree reduce?
-#inp4 = torch.stack((inp3[0], inp3[1] + inp3[2], torch.sum(torch.pow(inp3[1:],2))), dim=0)
-#inp5 = (inp4 * torch.tensor(Xscaler.scale_, dtype=torch.float64)) + torch.tensor(Xscaler.min_, dtype=torch.float64)
-#out1 = model(inp5)
-#out2 = (out1 * torch.tensor(yscaler.scale_, dtype=torch.float64)) + torch.tensor(yscaler.mean_, dtype=torch.float64)
-#print("Manual transformation energy: ",out2)
 
 def get_interatomics(xyz):
     # Build autodiff-OptKing internal coordinates of interatomic distances
