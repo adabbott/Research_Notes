49afbb4fd436fc99edf1a0ed21a33d1a2232fa81
adabbott <adabbott@uga.edu>
2019-08-03 16:40:36 -0400

    more functionality added. Still working on units/mass weighting of cubic terms

--- ML_forceconstants/water_1/derivatives.py
+++ ML_forceconstants/water_1/derivatives.py
@@ -13,6 +13,7 @@ amu2kg = 1.6605389e-27
 ang2m = 1e-10
 c = 29979245800.0 # speed of light in cm/s
 convert = np.sqrt(hartree2J/(amu2kg*ang2m*ang2m))/(c*2*np.pi)
+convert2 = np.sqrt(hartree2J/(amu2kg*amu2kg*ang2m*ang2m))/(c*2*np.pi)
 
 # Load NN model
 nn = NeuralNetwork('model_data/PES.dat', InputProcessor(''), molecule_type='A2B')
@@ -44,6 +45,10 @@ def cart2distances(cart):
                 count += 1
     return distances
 
+def cart2internals(cart, internals):
+    values = Btensors.ad_intcos.qValues(internals, cart)   # Generate internal coordinates from cartesians
+    return values
+      
 def differentiate_nn(energy, geometry, order=1):
     # The grad_tensor starts of as a single element, the energy. Then it becomes the gradient, hessian, cubic ... 
     # depending on value of 'order'
@@ -87,12 +92,11 @@ def cartesian_freq(hess, m):
     freqs = np.sqrt(cartlamda) * convert
     return freqs[:-6], cartL
 
-def internal_freq(hess, internals, geom, m):
+def internal_freq(hess, B1, m):
     """
     Get harmonic frequencies with GF method. 
     hess : numpy array of hessian in internal coordinates Hartree/ang^2
-    internals : list of internal coordinate definitions from ad_intcos, STRE, BEND, TORS objects same order as in Hessian
-    geom : Torch Tensor of cartesian coordinates in angstrom, requires_grad=True
+    B1 : 1st order B tensor in terms of internal coordinates in Hessian 
     m : numpy array of masses in amu
     Returns 
     -------
@@ -100,40 +104,75 @@ def internal_freq(hess, internals, geom, m):
     """
     m = np.repeat(m,3)
     M = 1 / m
-    B = Btensors.ad_btensor.autodiff_Btensor(internals, geom, order=1)
-    B = B.detach().numpy()
-    G = np.einsum('in,jn,n->ij', B, B, M)
+    #B = Btensors.ad_btensor.autodiff_Btensor(internals, geom, order=1)
+    #B = B.detach().numpy()
+    G = np.einsum('in,jn,n->ij', B1, B1, M)
     GF = G.dot(hess)
     intlamda, intL = np.linalg.eig(GF)
-    print('eigval',intlamda)
-    print('eigvec',intL)
     idx = intlamda.argsort()[::-1]
     intlamda = intlamda[idx]
     freqs = np.sqrt(intlamda) * convert
     return freqs, intL
 
-def cartHess2intHess(H, intcos, geom):
+def cartderiv2intderiv(derivative_tensor, B1):
     """
-    Converts Cartesian hessian to internal coordinates Hessian.
-    H is a numpy array of the hessian
-    intcos is a list of internal coordinate definitions from Btensors.ad_intcos
-    geom is a torch.Tensor of cartesian coordinates, requires_grad=True
-    """
-    B = Btensors.ad_btensor.autodiff_Btensor(intcos, geom, order=1)
-    B = B.detach().numpy()
-    G = np.dot(B, B.T)
-    Ginv = np.linalg.inv(G)
-    Atranspose = np.dot(Ginv, B)
-    Hq = np.dot(Atranspose, np.dot(H, Atranspose.T))
-    return Hq
+    Converts cartesian derivative tensor (gradient, Hessian, cubic, quartic ...) into internal coordinates
+    Only valid at stationary points for Hessians and above.
 
-# this is optimized tightly, but apparently model was not trained on true equilibrium, just approximate. Next geom is true geom the model was trained on
-#cartesians = torch.tensor([[0.0000000000,  1.0144292965, -0.0959637982],
-#                           [0.0000000000, -0.0959637982,  1.0144292965],
-#                           [0.0000000000,  0.0815344978,  0.0815344978]], dtype=torch.float64, requires_grad=True)
+    Parameters
+    ----------   
+    derivative_tensor : np.ndarray
+        Tensor of nth derivative in Cartesian coordinates 
+    B1 : np.ndarray
+        B-matrix converting Cartesian coordinates to internal coordinates
+    """
+    G = np.dot(B1, B1.T)
+    Ginv = np.linalg.inv(G)
+    A = np.dot(Ginv, B1)
+    dim = len(derivative_tensor.shape)
+    if dim == 1:
+        int_tensor = np.einsum('ia,a->i', A, derivative_tensor)
+    elif dim == 2:
+        int_tensor = np.einsum('ia,jb,ab->ij', A, A, derivative_tensor)
+    elif dim == 3:
+        int_tensor = np.einsum('ia,jb,kc,abc->ijk', A, A, A, derivative_tensor)
+    elif dim == 4:
+        int_tensor = np.einsum('ia,jb,kc,ld,abcd->ijkl', A, A, A, A, derivative_tensor)
+    else:
+        raise Exception("Too many dimensions. Add code to function")
+    return int_tensor
+
+def intderiv2cartderiv(derivative_tensor, B1):
+    """ 
+    Converts cartesian derivative tensor (gradient, Hessian, cubic, quartic ...) into internal coordinates
+    Only valid at stationary points for Hessians and above.
+
+    Parameters
+    ----------   
+    derivative_tensor : np.ndarray
+        Tensor of nth derivative in internal coordinates 
+    B1 : np.ndarray
+        B-matrix converting internal coordinates to Cartesian coordinates
+    """
+    dim = len(derivative_tensor.shape)
+    if dim == 1:
+        cart_tensor = np.einsum('ia,i->a', B1, derivative_tensor)
+    elif dim == 2:
+        cart_tensor = np.einsum('ia,jb,ij->ab', B1, B1, derivative_tensor)
+    elif dim == 3:
+        cart_tensor = np.einsum('ia,jb,kc,ijk->abc', B1, B1, B1, derivative_tensor)
+    elif dim == 4:
+        cart_tensor = np.einsum('ia,jb,kc,ld,ijkl->abcd', B1, B1, B1, B1, derivative_tensor)
+    else:
+        raise Exception("Too many dimensions. Add code to function to compute")
+    return cart_tensor
+
 cartesians = torch.tensor([[ 0.0000000000,0.0000000000,0.9496765298],
                            [ 0.0000000000,0.8834024755,-0.3485478124],
                            [ 0.0000000000,0.0000000000,0.0000000000]], dtype=torch.float64, requires_grad=True)
+cartesians_nograd = torch.tensor([[ 0.0000000000,0.0000000000,0.9496765298],
+                           [ 0.0000000000,0.8834024755,-0.3485478124],
+                           [ 0.0000000000,0.0000000000,0.0000000000]], dtype=torch.float64, requires_grad=False)
 #
 # HH H1O H2O
 eq_geom = [1.570282260121,0.949676529800,0.949676529800]
@@ -141,10 +180,10 @@ distances = torch.tensor(eq_geom, dtype=torch.float64, requires_grad=True)
 print("Equilibrium geometry and energy: ", eq_geom, pes(eq_geom,cartesian=False)) # Test computation
 
 # Compute internal coordinate Hessian wrt starting from both interatomic distances and cartesian coordinates, this works
-computed_distances = cart2distances(cartesians)
-E = transform(computed_distances)
-hint =  differentiate_nn(E, computed_distances, order=2)
-hcart =  differentiate_nn(E, cartesians, order=2)
+#computed_distances = cart2distances(cartesians)
+#E = transform(computed_distances)
+#hint =  differentiate_nn(E, computed_distances, order=2)
+#hcart =  differentiate_nn(E, cartesians, order=2)
 
 # Psi4 hessian and frequencies
 psi4.core.be_quiet()
@@ -194,13 +233,13 @@ psihess /= 0.529177249**2
 #print(quadratic)
 #print(quadratic * convert)
 
-def quadratic(m, hess, L, B):
-    """Mass of each atom, internal coordinate hessian, eigenvectors of hessian, B tensor"""
+def quadratic(hess, m, L, B):
+    """internal coordinate hessian, Mass of each atom,  eigenvectors of hessian, B tensor"""
     M = np.sqrt(1 / np.repeat(m,3))
     inv_trans_L = np.linalg.inv(L).T # Maybe dont tranpose? does inverse flip dimension? 
     little_l = np.einsum('a,ia,ir->ar', M, B, inv_trans_L)
     L1_tensor = np.einsum('ia,a,ar->ir', B, M, little_l)
-    # NORMALIZE DUMBASS 
+    # NORMALIZE 
     L1 = L1_tensor / np.linalg.norm(L1_tensor, axis=0)
     # MASS WEIGHT HESSIAN w/G THEN USE FORCE CONSTANT EQUATION
     G = np.einsum('in,jn,n,n->ij', B, B, M, M)
@@ -208,17 +247,76 @@ def quadratic(m, hess, L, B):
     quadratic = np.einsum('ij,ir,js->rs', GF, L1, L1)
     print('quad',np.sqrt(quadratic) * convert)
 
+def cubic(hess, cubic, m, L, B1, B2):
+    M = np.sqrt(1 / np.repeat(m,3))
+    inv_trans_L = np.linalg.inv(L).T # Maybe dont tranpose? does inverse flip dimension? 
+    little_l = np.einsum('a,ia,ir->ar', M, B1, inv_trans_L)
+
+    L1_tensor = np.einsum('ia,a,ar->ir', B1, M, little_l)
+    L1 = L1_tensor / np.linalg.norm(L1_tensor, axis=0)
+
+    L2_tensor = np.einsum('iab,a,ar,b,bs->irs', B2, M, little_l, M, little_l)
+    invnorm = np.reciprocal(np.linalg.norm(L2_tensor, axis=1))
+    # This normalization may be wrong.. but what else could it be?
+    L2 = np.einsum('ijk,ik->ijk', L2_tensor, invnorm)
+
+    # TODO: Mass weight Cubic tensor???
+    G1 = np.einsum('in,jn,n,n->ij', B1, B1, M, M)
+    mwhess = np.einsum('ij,jk->ik', G1,hess)
+    G2 = np.einsum('inm, jnm, knm, n, m->ijk', B2, B2, B2, M, M)
+    #G2 = np.einsum('in,jn,kn,n,n->ijk', B1, B1, B1, M, M)
+    #mwcubic = np.einsum('ijk,jkl->ikl', G2, cubic)
+    mwcubic = np.einsum('ijk,jkl->ikl', G2, cubic)
+    #mwcubic = G2.dot(cubic)
+    print(mwcubic.shape)
+    #GF = np.einsum('ij,jk->ik', G,hess)
+
+    term1 = np.einsum('ijk,ir,js,kt->rst', mwcubic, L1, L1, L1)
+    term2 = np.einsum('ij, irs, jt->rst', mwhess, L2, L1)
+    term3 = np.einsum('ij, irt, js->rst', mwhess, L2, L1)
+    term4 = np.einsum('ij,ist,jr->rst', mwhess, L2, L1)
+    fc_3 = term1 + term2 + term3 + term4
+
+    print(fc_3)
+    print(fc_3 * convert2)
+    print(np.sqrt(fc_3) * convert2)
+
 # Use Psi4 data first
-internals = [Btensors.ad_intcos.STRE(0,2), Btensors.ad_intcos.STRE(1,2), Btensors.ad_intcos.BEND(0,2,1)]
-psi4_inthess = cartHess2intHess(psihess, internals, cartesians)
+#internals = [Btensors.ad_intcos.STRE(0,2), Btensors.ad_intcos.STRE(1,2), Btensors.ad_intcos.BEND(0,2,1)]
+#psi4_inthess = cartHess2intHess(psihess, internals, cartesians)
 
-m = np.array([1.007825032230, 1.007825032230, 15.994914619570])
-f, L = internal_freq(psi4_inthess, internals, cartesians, m)
-print(f)
+#m = np.array([1.007825032230, 1.007825032230, 15.994914619570])
+#f, L = internal_freq(psi4_inthess, internals, cartesians, m)
+#print(f)
 
-B = Btensors.ad_btensor.autodiff_Btensor(internals, cartesians, order=1)
-B = B.detach().numpy()
-quadratic(m, psi4_inthess, L, B)
+#B1 = Btensors.ad_btensor.autodiff_Btensor(internals, cartesians, order=1)
+#B1 = B1.detach().numpy()
+#quadratic(m, psi4_inthess, L, B1)
+#cubic(psi4_inthess, L, B1, B2)
+
+
+
+# Define curvilinear internal coordinates, 1st, 2nd order B tensors
+
+# Genearte Cartesian Hessian with NN, convert to internal coordinates
+#internal_coordinates =  cart2internals(cartesians, internals)
+#print(internal_coordinates)
+#hess_curvi = differentiate_nn(E, ad_internals, order=2)
+#print(hess_curvi)
+#hess_cart =  differentiate_nn(E, cartesians, order=2)
+#hess_int = cartHess2intHess(hess_cart.detach().numpy(), internals, cartesians)
+#m = np.array([1.007825032230, 1.007825032230, 15.994914619570])
+#f, L = internal_freq(hess_int, internals, cartesians, m)
+#print(f)
+# Check: perform normal coordinate analysis, the L-tensor way
+#quadratic(hess_int, m, L, B1)
+
+#cubic_cart =  differentiate_nn(E, cartesians, order=3)
+#quartic_cart =  differentiate_nn(E, computed_distances, order=4)
+
+#hess_internals =  differentiate_nn(E, computed_distances, order=2)
+#cubic_internals =  differentiate_nn(E, computed_distances, order=2)
+
 
 # CHECK
 #G = np.einsum('in,jn,n,n->ij', B, B, M, M)
@@ -227,7 +325,49 @@ quadratic(m, psi4_inthess, L, B)
 
 
 
+# Some accuracy as lost here, probably due to the gradient not being exactly zero.
+#computed_distances = cart2distances(cartesians)
+#E = transform(computed_distances)
+#hess = differentiate_nn(E, cartesians, order=2)
+#print('cart hess')
+#print(hess)
+#print(cartesian_freq(hess.detach().numpy(), m))
+#print('converted cart hess to int hess')
+#inthess = cartderiv2intderiv(hess.detach().numpy(), B1)
+#print(inthess)
+#print('backtransformed to cart hess')
+#newcart = intderiv2cartderiv(inthess, B1)
+#print(newcart)
+#print(cartesian_freq(newcart, m))
+
+internals = [Btensors.ad_intcos.STRE(0,2), Btensors.ad_intcos.STRE(1,2), Btensors.ad_intcos.BEND(0,2,1)]
+interatomics = get_interatomics(3)
+B1, B2 = Btensors.ad_btensor.fast_B(internals, cartesians_nograd)
+B1, B2 = B1.detach().numpy(), B2.detach().numpy()
+B1_idm, B2_idm = Btensors.ad_btensor.fast_B(interatomics, cartesians_nograd)
+B1_idm, B2_idm = B1_idm.detach().numpy(), B2_idm.detach().numpy()
+ 
+m = np.array([1.007825032230, 1.007825032230, 15.994914619570])
+computed_distances = cart2distances(cartesians)
+E = transform(computed_distances)
+interatomic_hess = differentiate_nn(E, computed_distances, order=2).detach().numpy()
+cart_hess = intderiv2cartderiv(interatomic_hess, B1_idm)
+curvilinear_hess = cartderiv2intderiv(cart_hess, B1)
+
+interatomic_cubic = differentiate_nn(E, computed_distances, order=3).detach().numpy()
+cart_cubic = intderiv2cartderiv(interatomic_cubic, B1_idm)
+curvilinear_cubic = cartderiv2intderiv(cart_cubic, B1)
+
+f, L = internal_freq(curvilinear_hess, B1, m)
+quadratic(curvilinear_hess, m, L, B1)
+cubic(curvilinear_hess, curvilinear_cubic, m, L, B1, B2)
 
 
+
+
+# Identical frequencies, good!
+#idm_f, idm_L = internal_freq(interatomic_hess, B1_idm, m)
+#cart_f, cart_L = cartesian_freq(cart_hess, m)
+#f, L = internal_freq(curvilinear_hess, B1, m)
 
 
